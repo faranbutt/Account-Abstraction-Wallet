384be26302127b4e76a6ba594bfd498435540ece
faranbutt <faranbutt789@gmail.com>
2023-04-12 19:34:44 +0500

    Pushing Wallet

---
+++ app/components/rainbow.tsx
@@ -0,0 +1,57 @@
+'use client'
+import { getZeroDevSigner, getSocialWalletOwner } from '@zerodevapp/sdk'
+
+import { 
+  SocialWallet, 
+  GoogleSocialWallet, 
+  FacebookSocialWallet,
+  GithubSocialWallet,
+  DiscordSocialWallet,
+  TwitchSocialWallet,
+  TwitterSocialWallet
+} from '@zerodevapp/social-wallet';
+import { useState } from 'react';
+import { useMemo } from 'react';
+const socialWallet = new GoogleSocialWallet()
+function RpcProviderExample() {
+    const [address, setAddress] = useState('')
+    const [loading, setLoading] = useState(false)
+  
+    const socialWallet = useMemo(() => {
+      return new SocialWallet()
+    }, [])
+    
+    const createWallet = async () => {
+      setLoading(true)
+      const signer = await getZeroDevSigner({
+        projectId: '7c128cf6-b7da-4177-981a-9ed9954f493a',
+        owner: await getSocialWalletOwner('7c128cf6-b7da-4177-981a-9ed9954f493a', socialWallet)
+      })
+      setAddress(await signer.getAddress())
+      setLoading(false)
+    }
+  
+    const disconnect = async () => {
+      await socialWallet.disconnect()
+      setAddress('')
+    }
+  
+    const connected = !!address
+  
+    return (
+      <div>
+        {connected && 
+          <div>
+            <label>Wallet: {address}</label>
+          </div>
+        }
+        <div>
+          {!connected && <button onClick={createWallet} disabled={loading}>{ loading ? 'loading...' : 'Create Wallet'}</button>}
+          {connected && 
+            <button onClick={disconnect} disabled={loading}>Disconnect</button>
+          }
+        </div>
+      </div>
+    )
+  }
+export default RpcProviderExample;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import RpcProviderExample from "./components/rainbow"
+import EtherExample from "./components/ether"
+export default function Home() {
+  return (
+    <div>
+      <RpcProviderExample />
+      <EtherExample />
+    </div>
+  )
+}
